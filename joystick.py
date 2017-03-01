e57ca4f4254a869787e550998d44ee98b71f64f9
sevistatic <bryants620@gmail.com>
2017-03-01 20:14:28 +0000

    Joystick control includes deadzones and extracted methods

--- joystick.py
+++ joystick.py
@@ -1,15 +1,39 @@
 from evdev import InputDevice, categorize, ecodes
+
+def rotateGross(value, center_pos):
+	if value < (center_pos - 50):
+        	print("Coarse Rotate Left")
+	elif value >= (center_pos - 50) and value <= (center_pos + 50):
+		print("No Coarse Rotation")
+        else:
+		print("Coarse Rotate Right")
+#	print("Z_AXIS EVENT - VAL: {}".format(keyevent.event.value))		
+
+def rotateFine(value, center_pos):
+	if value < (center_pos - 25):
+		print("Fine Rotate Left")
+	elif value >= (center_pos - 25) and value <= (center_pos + 25):
+		print("No Fine Rotation")
+	else:
+		print("Fine Rotate Right")
+#	print("Y_AXIS EVENT - VAL: {}".format(keyevent.event.value))
+
+def tilt(value, center_pos):
+	if value < (center_pos - 50):
+        	print("Tilt Downward")
+	elif value <= (center_pos - 50) and value >= (center_pos + 50):
+		print("No Rotation")
+        else:
+		print("Tilt Upward")
+#	print("X_AXIS EVENT - VAL: {}".format(keyevent.event.value))
+
 gamepad = InputDevice('/dev/input/event0')
 for event in gamepad.read_loop():
 	if event.type == ecodes.EV_ABS:
-		keyevent = categorize(event)
-#		print("KEYEVENT: {}".format(keyevent))
-#		print("EVENT: {}".format(keyevent.event))
-#		print("CODE: {}".format(keyevent.event.code))
-#		print("VALUE: {}".format(keyevent.event.value))
-		if keyevent.event.code == 5:
-			print("Z_AXIS EVENT - VAL: {}".format(keyevent.event.value))		
-		elif keyevent.event.code == 1:
-			print("X_AXIS EVENT - VAL: {}".format(keyevent.event.value))
-		elif keyevent.event.code == 0:
-			print("Y_AXIS EVENT - VAL: {}".format(keyevent.event.value))
+		event = categorize(event).event
+		if event.code == 5:
+			rotateFine(event.value, 256)
+		elif event.code == 1:
+			tilt(event.value, 512)
+		elif event.code == 0:
+			rotateGross(event.value, 512)
